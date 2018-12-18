28c956c258a5b798a3a990ae6097333265d441b6
Huy Le <rqfw47@Huy-Les-Mac.am.mot-solutions.com>
2018-12-17 19:20:03 -0600

    init

---
+++ app/src/main/java/sample/huy/huy_retrofit_practice/activity/model/AlbumPhoto.kt
@@ -0,0 +1,20 @@
+package sample.huy.huy_retrofit_practice.activity.model
+
+class AlbumPhoto {
+    var id:Int = -1
+        get() = id
+        set(value) {
+            if(value > 0)
+                field = value
+        }
+    var title:String = ""
+
+    constructor(_id: Int, _title:String) {
+        id = _id
+        title = _title
+    }
+
+    companion object {
+        val TAG = "AlbumPhoto"
+    }
+}
